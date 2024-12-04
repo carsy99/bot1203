976294b158b658721671e087b0d0ad62d76f2b40
carsy99 <144758369+carsy99@users.noreply.github.com>
2024-12-04 09:41:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -73,12 +73,12 @@ def handle_message(event):
 
     elif user_message == "熱門音樂":  # 傳送熱門音樂音訊
         reply = AudioSendMessage(
-            original_content_url="https://www.dropbox.com/scl/fi/v5ujwe86ebkaxg64zv2v1/Selfish_Waltz_-_YouConvert.net_.mp3?rlkey=6fx95eyj4b983e31dyvgmldt4&st=p2a1zozd&dl=0",  
+            original_content_url="https://www.dropbox.com/scl/fi/v5ujwe86ebkaxg64zv2v1/Selfish_Waltz_-_YouConvert.net_.mp3",  
             duration=203000  # 音訊時長 (毫秒)
         )
     elif user_message == "放鬆音樂":  # 傳送放鬆音樂音訊
         reply = AudioSendMessage(
-            original_content_url="https://www.dropbox.com/scl/fi/ap9cmzl743c0rdw9oisvg/Back_2_U_AM_01_27_-_YouConvert.net_.mp3?rlkey=3mrd7mq5jw19iw494olg64qd8&st=migo8cqe&dl=0",  
+            original_content_url="https://www.dropbox.com/scl/fi/ap9cmzl743c0rdw9oisvg/Back_2_U_AM_01_27_-_YouConvert.net_.mp3",  
             duration=235000  # 音訊時長 (毫秒)
         )
     else:
